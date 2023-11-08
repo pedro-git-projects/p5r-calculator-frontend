9a9fc5e7cafb9c1731975dc513576d653fe953b5
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-11-08 00:12:58 -0300

    formatting code with prettier #3

--- src/components/PersonaTable.tsx
+++ src/components/PersonaTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface PersonaProps {
   persona: Persona;
@@ -15,7 +15,9 @@ const PersonaTable: React.FC<PersonaProps> = ({ persona }) => {
             <th className="px-2 py-1 text-sm bg-gray-200 border">Item</th>
             <th className="px-2 py-1 text-sm bg-gray-200 border">Itemr</th>
             <th className="px-2 py-1 text-sm bg-gray-200 border">Level</th>
-            <th className="px-2 py-1 text-sm bg-gray-200 border">Resistances</th>
+            <th className="px-2 py-1 text-sm bg-gray-200 border">
+              Resistances
+            </th>
             <th className="px-2 py-1 text-sm bg-gray-200 border">Skills</th>
             <th className="px-2 py-1 text-sm bg-gray-200 border">Stats</th>
             <th className="px-2 py-1 text-sm bg-gray-200 border">Trait</th>
@@ -29,9 +31,15 @@ const PersonaTable: React.FC<PersonaProps> = ({ persona }) => {
             <td className="px-2 py-1 border text-sm">{persona.item}</td>
             <td className="px-2 py-1 border text-sm">{persona.itemr}</td>
             <td className="px-2 py-1 border text-sm">{persona.lvl}</td>
-            <td className="px-2 py-1 border text-sm">{formatResistances(persona.resists)}</td>
-            <td className="px-2 py-1 border text-sm">{formatSkills(persona.skills)}</td>
-            <td className="px-2 py-1 border text-sm">{formatStats(persona.stats)}</td>
+            <td className="px-2 py-1 border text-sm">
+              {formatResistances(persona.resists)}
+            </td>
+            <td className="px-2 py-1 border text-sm">
+              {formatSkills(persona.skills)}
+            </td>
+            <td className="px-2 py-1 border text-sm">
+              {formatStats(persona.stats)}
+            </td>
             <td className="px-2 py-1 border text-sm">{persona.trait}</td>
             <td className="px-2 py-1 border text-sm">{persona.arcana}</td>
           </tr>
@@ -40,8 +48,6 @@ const PersonaTable: React.FC<PersonaProps> = ({ persona }) => {
     </div>
   );
 };
-
-
 
 const formatResistances = (resists: {
   phys: string;
@@ -56,34 +62,32 @@ const formatResistances = (resists: {
   curse: string;
 }) => {
   const resistanceLabels = {
-    phys: 'Phys',
-    gun: 'Gun',
-    fire: 'Fire',
-    ice: 'Ice',
-    elec: 'Elec',
-    wind: 'Wind',
-    pys: 'Pys',
-    nuke: 'Nuke',
-    bless: 'Bless',
-    curse: 'Curse',
+    phys: "Phys",
+    gun: "Gun",
+    fire: "Fire",
+    ice: "Ice",
+    elec: "Elec",
+    wind: "Wind",
+    pys: "Pys",
+    nuke: "Nuke",
+    bless: "Bless",
+    curse: "Curse",
   };
 
-  return Object.keys(resists).map((key) => (
+  return Object.keys(resists).map(key => (
     <td key={key} className="px-2 py-1 border">
       <strong>{resistanceLabels[key]}:</strong> {resists[key]}
     </td>
   ));
 };
 
-
 const formatSkills = (skills: { [key: string]: number }) => {
-  return Object.keys(skills).map((key) => (
+  return Object.keys(skills).map(key => (
     <td key={key} className="px-2 py-1 border">
       <strong>{key}:</strong> {skills[key]}
     </td>
   ));
 };
-
 
 const formatStats = (stats: {
   st: number;
@@ -93,21 +97,18 @@ const formatStats = (stats: {
   lu: number;
 }) => {
   const statLabels = {
-    st: 'Strength',
-    ma: 'Magic',
-    en: 'Endurance',
-    ag: 'Agility',
-    lu: 'Luck',
+    st: "Strength",
+    ma: "Magic",
+    en: "Endurance",
+    ag: "Agility",
+    lu: "Luck",
   };
 
-  return Object.keys(stats).map((key) => (
+  return Object.keys(stats).map(key => (
     <td key={key} className="px-2 py-1 border">
       <strong>{statLabels[key]}:</strong> {stats[key]}
     </td>
   ));
 };
 
-
-
 export default PersonaTable;
-
